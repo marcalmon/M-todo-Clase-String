ad63793be049ed2b334a43db6334e05c14c66857
Mar Calmon <marical2222@gmail.com>
2017-08-29 15:29:26 -0300

    Martes 29/0872017, Metodos de clase String

--- src/Metodo_IndexOf.java
+++ src/Metodo_IndexOf.java
@@ -1,5 +1,6 @@
-/**
- *
+/*El método indexOf retorna la posicion de la primera ocurrencia de un caracter
+ *dentro del String. Si la cadena no contiene ese caracter entonces retorna un
+ *valor negativo.
  * @author Ingles
  */
 public class Metodo_IndexOf {
@@ -7,11 +8,25 @@ public class Metodo_IndexOf {
     
     public static void main(String[] args){
     
-        String cadena="Hoy es un dia lluvioso";
+        String cadena="Hoy Es un Dia Lluvioso";
         
-        int pos=cadena.indexOf("d");
+        System.out.println(cadena.indexOf("D"));
         
-        System.out.println(pos);
+        System.out.println(cadena.toLowerCase());
+        
+        System.out.println(cadena.startsWith("H"));
+        
+        System.out.println(cadena.endsWith("e"));
+        
+        System.out.println(cadena.equals("Hoy Es un Dia Lluvioso"));
+        
+        System.out.println(cadena.toUpperCase());
+        
+        System.out.println(cadena.replace('o', 'e'));
+        
+        System.out.println(cadena.charAt(2));
+        
+        System.out.println(cadena.length());
     
     }
 }
