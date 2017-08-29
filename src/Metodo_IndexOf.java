91b2231eb2ce204f14ddb9c5b7dfcc59257bce48
Mar Calmon <marical2222@gmail.com>
2017-08-29 14:48:15 -0300

    Martes 29/0872017, Metodo IndexOf de Clase String

---
+++ src/Metodo_IndexOf.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Ingles
+ */
+public class Metodo_IndexOf {
+    
+    
+    public static void main(String[] args){
+    
+        String cadena="Hoy es un dia lluvioso";
+        
+        int pos=cadena.indexOf("dia");
+        
+        System.out.println(pos);
+    
+    }
+}
